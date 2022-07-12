c73eb004588a70fdae29992cad9e68606cdfc256
MattKohn <kohney@hotmail.com>
2022-07-12 12:27:18 -0600

    Make SignIn control form tracking user state

--- client/src/components/home/SignIn.jsx
+++ client/src/components/home/SignIn.jsx
@@ -1,10 +1,54 @@
-import { Box, styled } from '@mui/material'
-import React from 'react'
+import { Box, Button, styled, TextField } from '@mui/material'
+import React, { useState } from 'react'
 
 const SignIn = () => {
+  const [user, setUser] = useState({
+    username: '',
+    password: '',
+    password_confirmation: ''
+  })
+  console.log(user)
+
+  const handleUserInput = (e) => {
+    const inputName = e.target.name
+    setUser({
+      ...user, 
+      [inputName]: e.target.value
+    })
+  }
+
   return (
-    <SignInContainer>
-      <h1>Sign In / Signup Container</h1>
+    <SignInContainer component="form">
+
+      <h1>Sign Up to Order</h1>
+      <Credential 
+        autoFocus 
+        required 
+        label="username"
+        name="username"
+        type="username"
+        value={user.username}
+        onChange={handleUserInput}
+      />
+      <Credential 
+        required 
+        label="password"
+        name="password"
+        type="password"
+        value={user.password}
+        onChange={handleUserInput}
+      />
+      <Credential 
+        required 
+        label="confirm password"
+        name="password_confirmation"
+        type="password"
+        value={user.password_confirmation}
+        onChange={handleUserInput}
+      />
+      <SubmitBtn size="large" variant="solid">Submit</SubmitBtn>
+      
+
     </SignInContainer>
   )
 }
@@ -12,13 +56,27 @@ const SignIn = () => {
 export default SignIn
 
 const SignInContainer = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '20px',
+  textAlign: 'center',
   width: 500,
   height: 500,
   borderRadius: '20px',
   margin: '150px auto',
   backgroundColor: 'darkgreen',
   '&:hover': {
-    backgroundColor: 'lightgreen',
+    backgroundColor: 'green',
     opacity: [0.9, 0.8, 0.7]
   }
+})
+
+const Credential = styled(TextField)({
+  background: '#DDC',
+  borderRadius: '5px',
+  margin: '10px auto 50px'
+})
+
+const SubmitBtn = styled(Button)({
+
 })
