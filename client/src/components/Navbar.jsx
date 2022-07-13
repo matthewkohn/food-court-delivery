1e7affecc8f4b583d55d5a7ea1611f7da8e8768e
MattKohn <kohney@hotmail.com>
2022-07-13 15:27:14 -0600

    Add logout button to Navbar that logs out the user

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,54 @@
-import { AppBar, styled, Typography } from '@mui/material'
 import React from 'react'
+import { AppBar, IconButton, styled, Typography } from '@mui/material'
+import LogoutIcon from '@mui/icons-material/Logout';
 
-const Navbar = () => {
+const Navbar = ({ setUser }) => {
+
+  const handleLogout = (e) => {
+    fetch("/logout", {
+      method: "DELETE"
+    })
+      .then((res) => {
+        if (res.ok) {
+          setUser(null)
+        }
+      })
+  }
+
   return (
-    <AppBar>
+    <Banner>
       <Header variant="h3" component="div">Foodcourt Delivery</Header>
     
-    </AppBar>
+      <IconButton
+          size='large'
+          onClick={(e) => handleLogout(e)}
+      >
+        <Logout />
+      </IconButton>
+    </Banner>
   )
 }
 
 export default Navbar
 
+const Banner = styled(AppBar)({
+  display: 'flex',
+  flexDirection: 'row',
+  background: '#21BA54',
+  color: '#DDC'
+})
+
 const Header = styled(Typography)({
   padding: '10px',
-  flexGrow: 1,
-  background: '#21BA54'
+  flexGrow: 1
+})
+
+const Logout = styled(LogoutIcon)({
+  color: '#DDC',
+  margin: '10px',
+  '&:hover': {
+    // backgroundColor: 'darkgreen',
+    opacity: [0.9, 0.8, 0.7],
+    color: '#F16500'
+  }
 })
