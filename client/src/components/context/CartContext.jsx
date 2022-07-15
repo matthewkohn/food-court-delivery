0d629231990cd190b41eb2a78c1a8a0c2291ec10
MattKohn <kohney@hotmail.com>
2022-07-15 14:52:30 -0600

    Create CartContext

---
+++ client/src/components/context/CartContext.jsx
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from 'react'
+
+const CartContext = createContext()
+
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([])
+
+  const value = [cart, setCart]
+
+  return (
+    <CartContext.Provider value={ value }>
+      { children }
+    </CartContext.Provider>
+  )
+}
+
+export { CartContext, CartProvider }
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import { Container, styled } from '@mui/material'
 
 const Cart = () => {
 
 
   return (
-    <>
+    <CartContainer>
       <h1>CART</h1>
-    </>
+    </CartContainer>
     
   )
 }
 
 export default Cart
+
+
+const CartContainer = styled(Container)({
+  margin: '120px auto',
+  border: '3px solid #DDC',
+  borderRadius: '30px',
+  height: '60vh'
+})
