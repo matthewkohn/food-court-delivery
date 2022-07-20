8eee0bf6747a605fa9a36a943f92be88aec88a33
MattKohn <kohney@hotmail.com>
2022-07-20 16:36:15 -0600

    Fix bug in orders_controller create method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,36 +1,26 @@
 class OrdersController < ApplicationController
 
-
   def create
-    # when created, iterate through order_items and OrderItem.new each
-    # need to set :total and :item_count in each Order
-    # 
-    # byebug
-    @order = Order.new(user_id: session[:user_id])
-    # byebug
-    if @order.save
-      # byebug
-      params[:order_items].each do |item|
-        # byebug
-        OrderItem.create!(order_id: @order.id, item_id: item[:item_id], quantity: item[:quantity])
-      end
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
       render json: @order
     else
       render json: { error: "Bad request" }, status: 400
-    end 
-
+    end
   end
 
 
 
   private
 
-
-
-  # def order_params
-  #   params.permit(:order_items)
-  #       # params.permit(user_id: session[:user_id], :total, :item_count, :order_items)
-  # end
-
+  def order_params
+    params.permit(
+      :user_id, 
+      :total, 
+      :item_count, 
+      order_items_attributes: [:item_id, :unit_price, :quantity, :subtotal] 
+    )
+  end
 
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,9 +2,4 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
-  # Columns: :order_id, :item_id, :quantity, :unit_price and :subtotal
-  # 
-
-
-  
 end
